74fb9f4ead0d43164f85b86af1ca9e4ea0953d88
Bhoomeendra Singh Sisodiya <bhoomeendrasinghsisodiya@QMAC0367.local>
2020-03-09 18:13:17 +0530

    Impelemented Candidate Key function

--- Helper.py
+++ Helper.py
@@ -7,10 +7,8 @@ def Closer(table , attrSet):
     for i in range(len(closer)):
             for index in range(table.getNoOfFds()):
                 fd = table.getFdById(index)
-                fdLeft = fd[LEFT]
-                fdRight = fd[RIGHT]
-                if fdLeft.issubset(closer):
-                   closer = closer.union(fdRight)
+                if fd[LEFT].issubset(closer):
+                   closer.add(fd[RIGHT])
 
     return closer
 
@@ -25,12 +23,45 @@ def MinimalCover(table):
                 newFds[RIGHT].append(attr)
     #STEP 2
 
+    #STEP 3
 
 
 
 
 def CandidateKey(table):
-    pass;
+
+    essential = table.getAttr()
+    mayBeEssential = set()
+    notEssential = set()#will not be used
+
+    attrInLeft = set()
+    attrInRight = set()
+    for index in range(table.getNoOfFds()):
+        fd = table.getFdById(index)
+        attrInLeft.add(fd[LEFT])
+        attrInRight.add(fd[RIGHT])
+
+    mayBeEssential = attrInLeft.intersection(attrInRight)
+    essential = essential.difference(attrInRight)
+    notEssential= attrInRight.difference(attrInLeft)
+
+    closerofEssential = Closer(table,essential)
+
+    if closerofEssential == table.getAttr():
+        return essential
+    else:
+        pcOfMayBeEssential = possibleCombination(mayBeEssential)
+        setOfCandidateKeys = list()
+
+        for possiblity in pcOfMayBeEssential:
+
+            if Closer(table,essential.union(possiblity)) == table.getAttr() :
+                if isSupersetOfSetin(setOfCandidateKeys,essential.union(possiblity)) == False :
+                    setOfCandidateKeys.append(essential.union(possiblity))
+
+        return setOfCandidateKeys
+
+
 
 def possibleCombination(attrs):
     pC = []
@@ -38,6 +69,12 @@ def possibleCombination(attrs):
         pC.append(list(combinations(attrs,length)))
     return pC;
 
+def isSupersetOfSetin(setOfCandidateKeys , attrSet ):
+
+    for cd in setOfCandidateKeys:
+        if cd.issubset(attrSet):
+            return True;
+    return False
 
 
 
