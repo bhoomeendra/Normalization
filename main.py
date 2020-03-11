08503ddc98237a6f032e41f8d377fd646db86d0f
Bhoomeendra Singh Sisodiya <bhoomeendrasinghsisodiya@QMAC0367.local>
2020-03-11 17:40:45 +0530

    RestFul API /Decompose Added using Flask

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
 from Table import Table
 from Helper import possibleCombination, Closer, MinimalCover
+from flask import Flask,request,jsonify
+app = Flask(__name__)
+
+@app.route('/Decompose',methods=['POST'])
+def DecomposeTable():
+    attr = request.json['attribute']
+    fds  = request.json['fd']
+    print("Attributes: ",attr)
+    print("Functional Dependency: ",fds)
+    #Pasre this into fd list of list of set's
+    return jsonify(fds)
+
+if(__name__ == "__main__"):
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 attr = {"A", "B", "C", "D", "E"}
 fds = [[{"A", "B"}, {"D"}],[{"C"}, {"E"}]]
